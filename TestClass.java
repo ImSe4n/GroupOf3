a55c259047ee00a4d23980f2215f1f09bb59fafb
ImSe4n <snie7638@gmail.com>
2026-04-30 11:42:39 -0400

    hi

---
+++ TestClass.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class TestClass here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class TestClass
+{
+    public static void main(String[] args){
+        //hi
+    }
+}
